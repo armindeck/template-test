b946adcc61bf871d826cd868cc39f35a706e35e5
armindeck <dbhsoficial2003@gmail.com>
2026-07-13 21:52:40 -0500

    Vista del formulario para generar plantillas (Classic)

--- inc/form-view.php
+++ inc/form-view.php
@@ -22,6 +22,79 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+$filename = $filename ?? "";
+$type = $type ?? "classic";
+$version = $version ?? "";
+$render = $render ?? "";
+
+// View select type
+$is_classic_select = $type == "classic" ? " selected" : "";
+$is_standard_select = $type == "standard" ? " selected" : "";
+$is_modern_select = $type == "modern" ? " selected" : "";
+
+$view_select_type = <<<HTML
+    <label for="type" style="display: flex; justify-content: space-between; align-items: center; gap: 4px;">
+        <strong>Type:</strong>
+        <select name="type" id="type" onchange="window.location.href='?type='+this.value" required>
+            <option value="classic">--- Type ---</option>
+            <option value="classic"{$is_classic_select}>Classic</option>
+            <option value="standard"{$is_standard_select}>Standard</option>
+            <option value="modern"{$is_modern_select}>Modern</option>
+        </select>
+    </label>
+HTML;
+
+$view_field_filename = <<<HTML
+    <input type="text" name="filename" id="filename" value="{$filename}" placeholder="Filename: template-classic.json...">
+HTML;
+
+$view_field_version = <<<HTML
+    <input type="text" name="version" id="version" value="{$version}" placeholder="Version: (1.0.0, 1.1.0)...">
+HTML;
+
+$view_small_markdown_is_supported = <<<HTML
+    <small style="font-weight: bold; color: white; background-color: darkslategray; padding: 2px 6px; border-radius: 4px;">
+        Markdown is supported
+    </small>
+HTML;
+
+$view_small_commands_is_supported = <<<HTML
+    <small style="font-weight: bold; color: white; background-color: darkslategray; padding: 2px 6px; border-radius: 4px;">
+        Commands is supported
+    </small>
+HTML;
+
+$view_textarea_render = <<<HTML
+    <textarea name="render" id="render" cols="30" rows="10" style="flex: 1;" placeholder="<header>\n  {{ page.name }}\n</header>\n<main>\n  {{ main }}\n</main>\n<footer>\n  &amp;copy; {{ page.year }} {{ page.name }}.\n</footer>">{$render}</textarea>
+HTML;
+
+$view_label_markdown_is_supported = <<<HTML
+    <label for="render" style="display: flex; gap: 4px; flex-wrap: wrap;">
+        {$view_small_markdown_is_supported}
+    </label>
+HTML;
+
+$view_label_commands_is_supported = <<<HTML
+    <label for="render" style="display: flex; gap: 4px; flex-wrap: wrap;">
+        {$view_small_commands_is_supported}
+    </label>
+HTML;
+
+$view_label_markdown_and_commands_is_supported = <<<HTML
+    <label for="render" style="display: flex; gap: 4px; flex-wrap: wrap;">
+        {$view_small_markdown_is_supported}
+        {$view_small_commands_is_supported}
+    </label>
+HTML;
+
+$view_form_classic = <<<HTML
+    $view_select_type
+    $view_field_filename
+    $view_field_version
+    $view_label_markdown_and_commands_is_supported
+    $view_textarea_render
+HTML;
 ?>
 <style>
     * {
@@ -46,24 +119,7 @@ SOFTWARE.
 </style>
 <body>
     <form method="post">
-        <input type="text" name="id_file" id="id_file" placeholder="ID file: (template-classic.json)...">
-        <select name="type" id="type" required>
-            <option value="">--- Type ---</option>
-            <option value="classic">Classic</option>
-            <option value="standard">Standard</option>
-            <option value="Modern">Modern</option>
-        </select>
-        <input type="text" name="version" id="version" placeholder="Version: (1.0.0, 1.1.0)...">
-        <label for="render" style="display: flex; gap: 4px; flex-wrap: wrap;">
-            <small style="font-weight: bold; color: white; background-color: darkslategray; padding: 2px 6px; border-radius: 4px;">
-                Markdown is supported
-            </small>
-            <small style="font-weight: bold; color: white; background-color: darkslategray; padding: 2px 6px; border-radius: 4px;">
-                Commands is supported
-            </small>
-        </label>
-        <textarea name="render" id="render" cols="30" rows="10" style="flex: 1;" placeholder="Content: "></textarea>
-
+        <?= $type == "classic" ? $view_form_classic : "" ?>
         <button type="submit" name="save_template">Save</button>
     </form>
 </body>
