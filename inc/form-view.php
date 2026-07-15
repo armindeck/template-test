f12bb8bd7ee757cfaf26c0d3df2de20dedf1d241
armindeck <dbhsoficial2003@gmail.com>
2026-07-15 14:08:34 -0500

    Agregue el formulario para plantillas standard

--- inc/form-view.php
+++ inc/form-view.php
@@ -28,13 +28,16 @@ $type = $type ?? "classic";
 $version = $version ?? "";
 $render = $render ?? "";
 
+$containers = $containers ?? ["", ""];
+$count_containers = count($containers);
+
 // View select type
 $is_classic_select = $type == "classic" ? " selected" : "";
 $is_standard_select = $type == "standard" ? " selected" : "";
 $is_modern_select = $type == "modern" ? " selected" : "";
 
 $view_select_type = <<<HTML
-    <label for="type" style="display: flex; justify-content: space-between; align-items: center; gap: 4px;">
+    <label for="type" class="flex space-between items-center gap-8">
         <strong>Type:</strong>
         <select name="type" id="type" onchange="window.location.href='?type='+this.value" required>
             <option value="classic">--- Type ---</option>
@@ -46,11 +49,24 @@ $view_select_type = <<<HTML
 HTML;
 
 $view_field_filename = <<<HTML
-    <input type="text" name="filename" id="filename" value="{$filename}" placeholder="Filename: template-classic.json...">
+    <label class="flex flex-column gap-8">
+        <strong>Filename:</strong>
+        <input type="text" name="filename" id="filename" value="{$filename}" placeholder="Filename: template-classic.json...">
+    </label>
 HTML;
 
 $view_field_version = <<<HTML
-    <input type="text" name="version" id="version" value="{$version}" placeholder="Version: (1.0.0, 1.1.0)...">
+    <label class="flex space-between gap-8 items-center">
+        <strong>Versión:</strong>
+        <input type="text" name="version" id="version" value="{$version}" placeholder="Version: (1.0.0, 1.1.0)...">
+    </label>
+HTML;
+
+$view_field_count_containers = <<<HTML
+    <label class="flex space-between gap-8 items-center">
+        <strong>Containers:</strong>
+        <input type="number" name="count_containers" id="count_containers" value="{$count_containers}" min="0" max="99" placeholder="Containers: (1, 2, 3)...">
+    </label>
 HTML;
 
 $view_small_markdown_is_supported = <<<HTML
@@ -66,23 +82,45 @@ $view_small_commands_is_supported = <<<HTML
 HTML;
 
 $view_textarea_render = <<<HTML
-    <textarea name="render" id="render" cols="30" rows="10" style="flex: 1;" placeholder="<header>\n  {{ page.name }}\n</header>\n<main>\n  {{ main }}\n</main>\n<footer>\n  &amp;copy; {{ page.year }} {{ page.name }}.\n</footer>">{$render}</textarea>
+    <textarea name="render" id="render" cols="30" rows="10" class="flex-1" placeholder="<header>\n  {{ page.name }}\n</header>\n<main>\n  {{ main }}\n</main>\n<footer>\n  &amp;copy; {{ page.year }} {{ page.name }}.\n</footer>">{$render}</textarea>
 HTML;
 
+$view_textarea_render_standard = "";
+
+foreach ($containers as $key => $value) {
+    $container_key_plus = $key + 1;
+    $value_enabled = !empty($value["enabled"]) ? " selected" : "";
+    $value_render = secureString($value["render"] ?? "");
+
+    $view_textarea_render_standard .= <<<HTML
+        <fieldset class="flex flex-column gap-8 p-8">
+            <legend>Container {$container_key_plus}</legend>
+            <label for="enabled[]" class="flex space-between items-center gap-8">
+                <strong>Show:</strong>
+                <select name="enabled[]" id="enabled[]">
+                    <option value="">No</option>
+                    <option value="1"{$value_enabled}>Yes</option>
+                </select>
+            </label>
+            <textarea name="render[]" id="render[]" rows="12" placeholder="<header>\n  {{ page.name }}\n</header>\n<main>\n  {{ main }}\n</main>\n<footer>\n  &amp;copy; {{ page.year }} {{ page.name }}.\n</footer>">{$value_render}</textarea>
+        </fieldset>
+    HTML;
+}
+
 $view_label_markdown_is_supported = <<<HTML
-    <label for="render" style="display: flex; gap: 4px; flex-wrap: wrap;">
+    <label for="render" class="flex gap-4 flex-wrap">
         {$view_small_markdown_is_supported}
     </label>
 HTML;
 
 $view_label_commands_is_supported = <<<HTML
-    <label for="render" style="display: flex; gap: 4px; flex-wrap: wrap;">
+    <label for="render" class="flex gap-4 flex-wrap">
         {$view_small_commands_is_supported}
     </label>
 HTML;
 
 $view_label_markdown_and_commands_is_supported = <<<HTML
-    <label for="render" style="display: flex; gap: 4px; flex-wrap: wrap;">
+    <label for="render" class="flex gap-4 flex-wrap">
         {$view_small_markdown_is_supported}
         {$view_small_commands_is_supported}
     </label>
@@ -95,31 +133,22 @@ $view_form_classic = <<<HTML
     $view_label_markdown_and_commands_is_supported
     $view_textarea_render
 HTML;
+
+$view_form_standard = <<<HTML
+    $view_select_type
+    $view_field_filename
+    $view_field_version
+    $view_field_count_containers
+    $view_label_markdown_and_commands_is_supported
+    $view_textarea_render_standard
+HTML;
+
+echo file_exists(RAIZ . "assets/css/lite.css") ? "<style type=\"text/css\">" . (file_get_contents(RAIZ . "assets/css/lite.css") ?? "") . "</style>" : "";
 ?>
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-    }
-    body {
-        display: flex;
-        justify-content: center;
-        min-height: 100vh;
-    }
-    form {
-        display: flex;
-        flex: 1;
-        flex-direction: column;
-        gap: 8px;
-        padding: 8px 10px;
-    }
-    input, button, select, textarea {
-        padding: 8px 8px;
-    }
-</style>
 <body>
     <form method="post">
         <?= $type == "classic" ? $view_form_classic : "" ?>
+        <?= $type == "standard" ? $view_form_standard : "" ?>
         <button type="submit" name="save_template">Save</button>
     </form>
 </body>
